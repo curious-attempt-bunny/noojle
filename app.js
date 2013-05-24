bdcd6b89467f2c12e5196eecb2ba30d7590c5555
Merlyn Albery-Speyer <merlyn@newrelic.com>
2013-05-23 22:00:12 -0700

    Add basic search page

--- app.js
+++ app.js
@@ -2,6 +2,12 @@ var express = require('express');
 var app = express();
 var parser = require('noojle-parser');
 
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+
+app.get('/', function(req, res) {
+  res.render('search', {results: {}});
+});
 
 app.get('/packages/:package', function(req, res) {
   parser(req.params.package, function(error, detail) {
